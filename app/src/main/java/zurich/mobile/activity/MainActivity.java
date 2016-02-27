c6ccee7310a98d6316bd5e0cde87df72992b7a66
zurich <zurich37@sina.com>
2016-02-27 21:42:39 +0800

    first commit

---
+++ app/src/main/java/zurich/mobile/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package zurich.mobile.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import zurich.mobile.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
